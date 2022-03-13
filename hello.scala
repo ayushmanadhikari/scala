06918ab2560b0133c4dfeff51136ba66fc5335e2
ayushman <unixparadoxfb@gmail.com>
2022-03-13 10:46:34 +0545

    new

--- hello.scala
+++ hello.scala
@@ -3,6 +3,6 @@ object Hello extends App{
   println("Hello World")
 }
 
-def sum(a: Int, b: Int): Int = a+b
-def concatenate(s1: String, s2: String): String = s1+s2
+//def sum(a: Int, b: Int): Int = a+b
+//def concatenate(s1: String, s2: String): String = s1+s2
 
